e20b3eabefebdc5dbc83952d8f7c0edfda0e1705
mugumori <mugumori@gmail.com>
2023-07-05 19:25:06 +0900

    Add terraform resources

---
+++ terraform/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.6.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
